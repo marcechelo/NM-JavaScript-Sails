756dfd0967586355fa2f9af281943a1da2e06577
Marcelo Nieto <marcechelo25@hotmail.com>
2018-06-19 15:28:07 -0500

    conectar servidores

--- Raspberry/config/bootstrap.js
+++ Raspberry/config/bootstrap.js
@@ -9,26 +9,32 @@
  * https://sailsjs.com/config/bootstrap
  */
 
-module.exports.bootstrap = async function(done) {
+const request = require('request');
 
-  // By convention, this is a good place to set up fake data during development.
-  //
-  // For example:
-  // ```
-  // // Set up fake development data (or if we already have some, avast)
-  // if (await User.count() > 0) {
-  //   return done();
-  // }
-  //
-  // await User.createEach([
-  //   { emailAddress: 'ry@example.com', fullName: 'Ryan Dahl', },
-  //   { emailAddress: 'rachael@example.com', fullName: 'Rachael Shaw', },
-  //   // etc.
-  // ]);
-  // ```
+module.exports.bootstrap = async function (done) {
 
-  // Don't forget to trigger `done()` when this bootstrap function's logic is finished.
-  // (otherwise your server will never lift, since it's waiting on the bootstrap)
+  setInterval(
+    () => {
+      console.log('Enviar datos');
+
+      const objetoRequest = {
+        url: 'http://localhost:1337/Usuario',
+        form: {
+          nombre: 'Adrian',
+          apellido: 'Eguez'
+        }
+      };
+      request
+        .post(objetoRequest,
+          (err, respuesta, body) => {
+            console.log('err', err);
+            console.log('respuesta', respuesta);
+            console.log('body', body);
+
+          })
+    }
+    , 10000);
+
   return done();
 
 };
